5f743369b187e35b646a55911140d6c1a8097308
adriana oliveira <457419@sga.pucminas.br>
2026-05-21 20:24:43 -0300

    dados

---
+++ assets/js/home.js
@@ -0,0 +1,34 @@
+import {
+    dados_das_noticias} from "./bd.js";
+
+    let vetor_das_noticias = [];
+    const container_das_noticias = document.getElementById ("noticias");
+    const opcao_noticia = document.getElementById ("opcao-noticia");
+    opcao_noticia.addEventListener ("Click", (e) => {
+        alternarApresentacaoDasNoticias();
+    })
+
+    function alternarApresentacaoDasNoticias() {
+        if (vetor_das_noticias.length == 0) {
+            carregarNoticias(); 
+            apresentarNoticias();
+        } else {
+            container_das_noticias.innerHTML  = ""; 
+            vetor_das_noticias = ();
+        }
+    }
+
+    const carregarNoticias = async () => {
+        vetor_das_noticias = dados_das_noticias;
+    };
+    function apresentarNoticias () {
+        const cartao = document.createElement ("div");
+        cartao.classname = "cartao";
+    }
+
+        cartao.innerHTML = `
+        <img src= "$(noticia.imagem)" alt= "$(noticia.titulo)" class= "imagem-do-cartao">
+        <section class= "corpo-do-cartao">
+        <h1 class= "titulo-do-cartao">  $(noticia.titulo)</h1> 
+        <p class = "classificacao">
+        <span class= "categoria-da-noticia" "$(noticia.categoria)" </span>
